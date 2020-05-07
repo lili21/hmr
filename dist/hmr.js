abfa4e8ed1610998ad121aa388b8f796c5d042ab
li.li <mark.li210812@gmail.com>
2020-05-08 02:06:54 +0800

    style: comment

--- dist/hmr.js
+++ dist/hmr.js
@@ -7,10 +7,6 @@
   // listen for 'file-change' message
   socket.on('file-change', function(msg) {
     console.log('File changed: ' + msg.id);
-    // reload the browser to get the latest changes
-    // we will replace this later to "hot update"
-    // only the changed modules
-    // window.location.reload();
     var script = document.createElement('script')
     script.src = `/hot-update?moduleId=${msg.id}`
     document.head.appendChild(script)
